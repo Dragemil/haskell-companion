083871a888b94c035bae2c1e023b5c064804f9a2
Emil Draganczuk <draganczuke@student.mini.pw.edu.pl>
2020-05-10 11:03:26 +0200

    Create initial draft

---
+++ HoogleDocs.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+
+import Control.Monad (when)
+import Data.Text (isPrefixOf, toLower, Text)
+import Control.Concurrent (threadDelay)
+import qualified Data.Text.IO as TIO
+
+import Discord
+import Discord.Types
+import qualified Discord.Requests as R
+
+-- | Replies "pong" to every message that starts with "ping"
+pingpongExample :: IO ()
+pingpongExample = do userFacingError <- runDiscord $ def
+            { discordToken = "Bot ZZZZZZZZZZZZZZZZZZZ"
+            , discordOnEvent = eventHandler }
+    TIO.putStrLn userFacingError
+
+eventHandler :: DiscordHandle -> Event -> IO ()
+eventHandler dis event = case event of
+    MessageCreate m -> when (not (fromBot m) && isPing (messageText m)) $ do
+        _ <- restCall dis (R.CreateReaction (messageChannel m, messageId m) "eyes")
+        threadDelay (4 * 10^6)
+        _ <- restCall dis (R.CreateMessage (messageChannel m) "Pong!")
+        pure ()
+    _ -> pure ()
+
+fromBot :: Message -> Bool
+fromBot m = userIsBot (messageAuthor m)
+
+isPing :: Text -> Bool
+isPing = ("ping" `isPrefixOf`) . toLower
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # haskell-companion
+
 A Discord bot that provides Haskell related documentation and news
